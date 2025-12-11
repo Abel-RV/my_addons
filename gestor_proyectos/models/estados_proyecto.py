9a3475ee788c569909c6ca7b6924b50adb8ee26f
Abel-RV <ramirezvillarejoabel@gmail.com>
2025-12-11 13:53:15 +0100

    Se ha hecho un cambio

--- gestor_proyectos/models/estados_proyecto.py
+++ gestor_proyectos/models/estados_proyecto.py
@@ -1,17 +1,17 @@
-# from odoo import models, fields, api
+    from odoo import models, fields, api
 
 
-# class gestor_proyectos(models.Model):
-#     _name = 'gestor_proyectos.gestor_proyectos'
-#     _description = 'gestor_proyectos.gestor_proyectos'
+    class estado_proyectos(models.Model):
+        _name = 'gestor_proyectos.gestors'
+        _description = 'gestor_proyectos.gestor_proyectos'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+        name = fields.Char()
+        value = fields.Integer()
+        value2 = fields.Float(compute="_value_pc", store=True)
+        description = fields.Text()
+
+#   @api.depends('value')
+#    def _value_pc(self):
+#        for record in self:
+#            record.value2 = float(record.value) / 100
 
