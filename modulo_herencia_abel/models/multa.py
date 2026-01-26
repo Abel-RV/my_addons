91f13c12722ee2e5cd612364d46751adc3f936f6
Abel-RV <ramirezvillarejoabel@gmail.com>
2026-01-26 10:15:16 +0100

    Cambios cosas

--- modulo_herencia_abel/models/multa.py
+++ modulo_herencia_abel/models/multa.py
@@ -1,17 +1,22 @@
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class modulo_herencia_abel(models.Model):
-#     _name = 'modulo_herencia_abel.modulo_herencia_abel'
-#     _description = 'modulo_herencia_abel.modulo_herencia_abel'
+    class Multa(models.Model):
+        _name = 'gestion.multa' # Nombre técnico del modelo en la base de datos
+        _description = 'Multas de Empleados' # Descripción del modelo
+        # Campo para la fecha en que se impone la multa
+        fecha = fields.Date(string="Fecha de la multa", required=True)
+        # Campo de texto para describir la razón de la multa
+        descripcion = fields.Text(string="Descripción", required=True)
+        # Relación Many2one con el modelo 'gestion.tipo_multa' (tipo de sanción)
+        tipo_multa_id = fields.Many2one('gestion.tipo_multa', string="Tipo de sanción", required=True)
+        # Relación Many2one con el modelo 'hr.employee' (empleado que recibe la multa)
+        empleado_id = fields.Many2one('hr.employee', string="Empleado involucrado", required=True)
+        # Campo para almacenar el importe monetario de la multa
+        importe = fields.Float(string="Importe de la multa")
+        # Campo booleano para indicar si la empresa ha pagado la multa
+        pagada = fields.Boolean(string="Pagada por la empresa", default=False)
+        # Campo calculado basado en el nombre del empleado
+        nombre_empleado = fields.Char(string="Nombre Empleado", related='empleado_id.name', store=False)
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
 
